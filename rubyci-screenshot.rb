552ab71fda4eed93c6cd490efb2304cc7002f43d
Yusuke Endoh <mame@ruby-lang.org>
2021-12-06 20:32:58 +0900

    Make it work again

--- rubyci-screenshot.rb
+++ rubyci-screenshot.rb
@@ -15,6 +15,9 @@ Selenium::WebDriver::Chrome::Service.driver_path = "/snap/bin/chromium.chromedri
 
 options = Selenium::WebDriver::Chrome::Options.new
 options.add_argument("--headless")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--no-sandbox")
 options.add_argument("--window-size=1280,3000")
 driver = Selenium::WebDriver.for(:chrome, options: options)
 
